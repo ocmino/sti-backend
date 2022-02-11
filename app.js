588242b2590e371f69ba0a5bfa6df88c59f88baa
Jonathan Lindqvist <ocmino@gmail.com>
2022-02-11 10:42:08 +0100

    Prepared...

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use('/healthcheck', require('/routes/healthcheck.routes'));
+app.use('/healthcheck', require('./routes/healthcheck.routes'));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 
